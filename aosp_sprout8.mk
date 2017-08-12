bf82ee7dc50c2579c07b094c3b01bc764748dba2
Samar Vispute <samarvispute121@gmail.com>
2017-08-12 22:45:03 +0530

    sprout8: AOSP-Bringup

--- aosp_sprout8.mk
+++ aosp_sprout8.mk
@@ -1,11 +1,11 @@
 $(call inherit-product, device/google/sprout8/sprout8.mk)
 
 # Common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=6.0/MRA58M/2280749:user/release-keys PRIVATE_BUILD_DESC="sprout-user 6.0 MRA58M 2280749 release-keys"
 
-PRODUCT_NAME := lineage_sprout8
+PRODUCT_NAME := aosp_sprout8
 PRODUCT_DEVICE :=sprout8
 PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
